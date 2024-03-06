257e103c046ea6181080f0a5a029206ca21d82aa
Manuel Quarneti <manuelquarneti@protonmail.com>
2024-03-06 11:57:40 +0100

    💄 Use noto icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true
   },
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), icon()]
 });
