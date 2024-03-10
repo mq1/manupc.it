415e8c2632840649826e78b781b8e336243281b3
Manuel Quarneti <manuelquarneti@protonmail.com>
2024-03-10 18:57:37 +0100

    🏷️ Typing

--- src/components/Configuratore.svelte
+++ src/components/Configuratore.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-  import bases from "../data/bases.json";
-  import attributes from "../data/attributes.json";
+  import { attributes } from "../data/attributes";
+  import type { Base } from "../data/bases";
 
-  export let pc: string;
-
-  let properties = bases[pc];
+  export let base: Base;
 
   let priceMap = {};
 
   const sendEmail = () => {
-    let text = `mailto:manu@manupc.it?subject=Ordine&body=Ciao, vorrei ordinare questo PC<br><br>${properties.name}`;
+    let text = `mailto:manu@manupc.it?subject=Ordine&body=Ciao, vorrei ordinare questo PC<br><br>${base.name}`;
 
     if (priceMap !== undefined) {
       Object.entries(priceMap).forEach(([attribute, selected]) => {
@@ -17,26 +15,36 @@
       });
     }
 
-    window.location.href = text
+    window.location.href = text;
   };
 
-  $: total =
-    properties.price +
-    Object.values(priceMap).reduce(
-      (accumulator, currentValue) => accumulator + currentValue[1],
-      0,
-    );
+  const getTotal = () => {
+    let total = base.price;
+
+    Object.entries(priceMap).forEach(([attribute, selected]) => {
+      total += attributes[attribute].options[selected];
+    });
+
+    return total;
+  };
+
+  let total = getTotal();
 </script>
 
 <div class="flex flex-col gap-2">
-  <p>PC base: {properties.price} €</p>
+  <p>PC base: {base.price} €</p>
 
-  {#each properties.attributes as attribute}
+  {#each base.attributes as attribute}
     <hr class="text-neutral-500 my-2" />
     <p>{attributes[attribute].label}</p>
-    <select bind:value={priceMap[attribute]}>
+    <select
+      bind:value={priceMap[attribute]}
+      on:change={() => {
+        total = getTotal();
+      }}
+    >
       {#each Object.entries(attributes[attribute].options) as [option, price]}
-        <option value={[option, price]}>{option} (+{price} €)</option>
+        <option value={option}>{option} (+{price} €)</option>
       {/each}
     </select>
   {/each}
@@ -44,7 +52,9 @@
   <hr class="text-neutral-500 my-8" />
   <p class="text-center">Totale: {total} € + 10€ di spedizione</p>
 
-  <button class="btn btn-primary" on:click={sendEmail}>Contattami per ordinarlo</button>
+  <button class="btn btn-primary" on:click={sendEmail}
+    >Contattami per ordinarlo</button
+  >
   <p class="text-sm text-center">
     O invia una email a <a href="mailto:manu@manupc.it" class="text-primary"
       >manu@manupc.it</a
